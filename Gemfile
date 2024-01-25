db6e84b9357d954ebeda916da4b670aa3c55b47e
Mikiko9882 <teddy.0802.t@gmail.com>
2024-01-25 23:05:03 +0900

    paginate

--- Gemfile
+++ Gemfile
@@ -48,6 +48,8 @@ gem 'draper', '3.1.0'
 
 gem 'carrierwave', '2.0.2'
 
+gem 'kaminari', '1.1.1'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/test_results_controller.rb
+++ app/controllers/test_results_controller.rb
@@ -2,7 +2,7 @@ class TestResultsController < ApplicationController
   before_action :find_test_result, only: [:edit, :update, :destroy]
 
   def index
-    @test_results = TestResult.all.includes(:user).order(created_at: :desc)
+    @test_results = TestResult.all.includes(:user).order(created_at: :desc).page(params[:page])
   end
 
   def new
