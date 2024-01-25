b32f1496ea208f78d5f437764fc4af2f7adbc26c
Mikiko9882 <teddy.0802.t@gmail.com>
2024-01-25 22:47:33 +0900

    タイトルを動的にする

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,14 @@ module ApplicationHelper
                 
     grades.product(classes).map { |grade, klass| ["#{grade.strip} #{klass.strip}", "#{grade.strip} #{klass.strip}"] }
   end
+
+  def page_title(page_title = '', admin = false)
+    base_title = if admin
+                  'Test Score Rivals(管理画面)'
+                else
+                  'Test Score Rivals'
+                end
+
+    page_title.empty? ? base_title : page_title + ' | ' + base_title
+  end
 end
